0f04123d2f8bf26d651a92682f43aa24fd0b88e1
Denny Park <park15dennis@gmail.com>
2020-11-16 14:24:26 -0800

    initial upload

---
+++ index.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line strict
+function main() {
+
+  $('#js-shopping-list-form').submit(function (event) {
+    event.preventDefault();
+    let list = ($(event.currentTarget).find('input[name="shopping-list-entry"]').val());
+    $('ul').append(
+      `<li>
+        <span class="shopping-item">${list}</span>
+        <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`
+    );
+  });
+}
+
+$('ul').on('click', '.shopping-item-delete', function (event) {
+  $(this).closest('li').remove();
+});
+$('ul').on('click', '.shopping-item-toggle', function (event) {
+  $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+});
+
+
+// eslint-disable-next-line no-undef
+$(main);
